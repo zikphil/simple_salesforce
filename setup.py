838bf8fb5ebdb48581f8c05383b70a6828252842
Philippe Labat <philippe.labat@me.com>
2021-06-21 15:05:32 -0400

    Added Async code

--- setup.py
+++ setup.py
@@ -4,13 +4,22 @@ import os
 import sys
 import textwrap
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
+
+package_name = 'simple_salesforce'
 
 about = {}
 with open(os.path.join(here, 'simple_salesforce', '__version__.py'), 'r') as f:
     exec(f.read(), about)
+
+
+packages = [
+    package
+    for package in find_packages(where=".", exclude=("test_elasticsearch*",))
+    if package == package_name or package.startswith(package_name + ".")
+]
 
 setup(
     name=about['__title__'],
@@ -19,12 +28,13 @@ setup(
     author_email=about['__author_email__'],
     maintainer=about['__maintainer__'],
     maintainer_email=about['__maintainer_email__'],
-    packages=['simple_salesforce',],
     url=about['__url__'],
     license=about['__license__'],
     description=about['__description__'],
     long_description=textwrap.dedent(open('README.rst', 'r').read()),
     long_description_content_type='text/x-rst',
+    packages=packages,
+    package_data={"simple_salesforce": ["*.py"]},
     install_requires=[
         'requests>=2.22.0',
         'authlib',
